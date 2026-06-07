3f50892b473c82e3816c7a67e62234fe6ea587a0
ASHUTOSH DUBEY <ashutoshdubey231001@gmail.com>
2026-06-07 19:02:55 +0000

    Jun 8, 2026, 12:32 AM

--- LEET CODE/q53.cpp
+++ LEET CODE/q53.cpp
@@ -5,7 +5,7 @@
 int maxsubarray(std::vector<int>&nums){
     int max_sum=nums[0];
     int current_sum=nums[0];
-    for(size_t i=1;i<nums.size();++1){
+    for(size_t i=1;i<nums.size();++i){
         current_sum=std::max(nums[i],current_sum+nums[i]);
         max_sum=std::max(max_sum,current_sum);
     }
