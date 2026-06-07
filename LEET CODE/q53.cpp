5f78950b60712a31de97393a855ea7b963e4ecf7
ASHUTOSH DUBEY <ashutoshdubey231001@gmail.com>
2026-06-07 19:07:57 +0000

    Jun 8, 2026, 12:37 AM

--- LEET CODE/q53.cpp
+++ LEET CODE/q53.cpp
@@ -1,14 +1,30 @@
 //Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
-#include<vector>
-#include<algorithm>
-int maxsubarray(std::vector<int>&nums){
-    int max_sum=nums[0];
-    int current_sum=nums[0];
-    std::cout<<"give me numbers" << std::endl; 
-    for(size_t i=1;i<nums.size();++i){
-        current_sum=std::max(nums[i],current_sum+nums[i]);
-        max_sum=std::max(max_sum,current_sum);
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int maxSubarray(const std::vector<int>& nums) {
+    int max_sum = nums[0];
+    int current_sum = nums[0];
+    for (size_t i = 1; i < nums.size(); ++i) {
+        current_sum = std::max(nums[i], current_sum + nums[i]);
+        max_sum = std::max(max_sum, current_sum);
     }
-    return max_sum; 
+    return max_sum;
+}
+
+int main() {
+    int n;
+    if (!(std::cin >> n) || n <= 0) {
+        return 0;
+    }
+
+    std::vector<int> nums(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> nums[i];
+    }
+
+    std::cout << maxSubarray(nums) << std::endl;
+    return 0;
 }
