312dcf6d79ab31ac703b31815b0f9564958eed04
ASHUTOSH DUBEY <ashutoshdubey231001@gmail.com>
2026-06-08 04:16:39 +0000

    Jun 8, 2026, 9:46 AM

--- YT SERIES/array ac/largest.cpp
+++ YT SERIES/array ac/largest.cpp
@@ -5,6 +5,7 @@ int main(){
     int size=6;
     int largest=0;
     largest=INT_MIN;
+    int smallest
     for(int i=0;i<size;i++){
         largest=max(largest,nums[i]);   
     }   cout<<"largest element is "<<largest<<endl;
