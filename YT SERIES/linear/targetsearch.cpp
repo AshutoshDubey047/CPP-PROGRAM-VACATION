5798191428d50ceab02389a34cdfedaed88a8f5f
ASHUTOSH DUBEY <ashutoshdubey231001@gmail.com>
2026-06-08 05:00:47 +0000

    Jun 8, 2026, 10:30 AM

--- YT SERIES/linear/targetsearch.cpp
+++ YT SERIES/linear/targetsearch.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int arr[]={1,2,3,4,5,6,7,8,9};
-    int target=5;
+    int target=11;
     int size=9;
     for(int i=0;i<size;i++){
         if(arr[i]==target){
