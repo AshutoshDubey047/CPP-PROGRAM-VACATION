3d945b13411220a8229e315aae65f4623260a4a8
ASHUTOSH DUBEY <ashutoshdubey231001@gmail.com>
2026-06-08 04:53:56 +0000

    Jun 8, 2026, 10:23 AM

--- YT SERIES/linear/targetsearch.cpp
+++ YT SERIES/linear/targetsearch.cpp
@@ -4,5 +4,10 @@ int main(){
     int arr[]={1,2,3,4,5,6,7,8,9};
     int target=5;
     int size=9;
-    
+    for(int i=0;i<size;i++){
+        if(arr[i]==target){
+            cout<<"target found at index "<<i<<endl;
+            return 0;
+        }
+    }   cout<<"target not found in the array"<<endl;
 }
