0ead0e6b39bcf8fec61e1c17275f4a1ad9a4b2df
ASHUTOSH DUBEY <ashutoshdubey231001@gmail.com>
2026-06-08 04:14:23 +0000

    Jun 8, 2026, 9:44 AM

--- YT SERIES/array ac/largest.cpp
+++ YT SERIES/array ac/largest.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int nums[] = {5,15,22,1,-15,-24};
     int size=6;
-    int largest=6;
+    int largest=0;
     int largest=INT_MIN;
     for(int i=0;i<size;i++){
         largest=max(largest,nums[i]);   
