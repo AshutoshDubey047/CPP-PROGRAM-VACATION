27ce4e35a5073c82b36801373bfab6f1533d468a
ASHUTOSH DUBEY <ashutoshdubey231001@gmail.com>
2026-06-09 04:27:29 +0000

    Jun 9, 2026, 9:57 AM

--- YT SERIES/array/sum&product.cpp
+++ YT SERIES/array/sum&product.cpp
@@ -1,2 +1,12 @@
 #include<iostream>
 using namespace std;
+int main(){
+    int nums[] = {5,15,22,1,-15,-24};
+    int size=6;
+    int sum=0;
+    int product=1;
+    for(int i=0;i<size;i++){
+        sum+=nums[i];
+        product*=nums[i];
+    }   cout<<"sum of the array is "<<sum<<endl;
+    cout<<"product of the array is "<<product<<endl;}
