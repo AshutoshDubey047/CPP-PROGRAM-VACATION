bef8b9c5d6ba1ebfeb188cb224e1452ef6d74923
ASHUTOSH DUBEY <ashutoshdubey231001@gmail.com>
2026-06-09 04:08:48 +0000

    Jun 9, 2026, 9:38 AM

--- YT SERIES/array/reverse.cpp
+++ YT SERIES/array/reverse.cpp
@@ -13,6 +13,23 @@ void reverse(int arr[],int size){
 
 
 int main(){
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
 
+    cout << "Original array: ";
+    for(int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 
+    reverse(arr, size);
+
+    cout << "Reversed array: ";
+    for(int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
 }
