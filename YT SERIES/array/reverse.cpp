2d03ae0f665f3e20ff10b27cafc3fa700758c58f
ASHUTOSH DUBEY <ashutoshdubey231001@gmail.com>
2026-06-09 04:08:33 +0000

    Jun 9, 2026, 9:38 AM

--- YT SERIES/array/reverse.cpp
+++ YT SERIES/array/reverse.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 using namespace std;
 
-void
+void reverse(int arr[],int size){
+    int start=0;
+    int end=size-1;
+    while(start<end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+}
 
 
 int main(){
