68a45c20b47a8102e9a7a63f2a1e2d61e47f0622
ASHUTOSH DUBEY <ashutoshdubey231001@gmail.com>
2026-06-10 03:42:38 +0000

    Jun 10, 2026, 9:12 AM

--- SELF PRATICE/first.cpp
+++ SELF PRATICE/first.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-using namespace std;
+// using namespace std;
 int main(){
-    cout<<"hello world";
-    cout<<"hello world";
-    cout<<"hello world";
-    cout<<"hello world";
-     cout<<"hello world";
-    cout<<"hello world";
-    cout<<"hello world";
-    cout<<"hello world" <<endl; 
+    // cout<<"hello world";
+    // cout<<"hello world";
+    // cout<<"hello world";
+    // cout<<"hello world";
+    //  cout<<"hello world";
+    // cout<<"hello world";
+    // cout<<"hello world";
+    // cout<<"hello world" <<endl; 
     std::cout<< "standard output";
     return 0;
 }
