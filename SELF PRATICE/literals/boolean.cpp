c5379dbf03c903bac62d8c3c50d073c2672fc08a
ASHUTOSH DUBEY <ashutoshdubey231001@gmail.com>
2026-06-10 05:05:23 +0000

    Jun 10, 2026, 10:35 AM

--- SELF PRATICE/literals/boolean.cpp
+++ SELF PRATICE/literals/boolean.cpp
@@ -7,5 +7,11 @@ int main(){
         cout<<"This is true"<<endl;
     }   else{
         cout<<"This is false"<<endl;
-    }   
+    }       
+        if(isFalse){
+            cout<<"This is true"<<endl;
+        }   else{
+            cout<<"This is false"<<endl;
+        }       
+        return 0;   
 }
